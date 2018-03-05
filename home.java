b9bdb6484f9501bb1d786204b6610c9bdf3bf638
Nikhil Anand <imnikhilanand@gmail.com>
2018-03-06 00:49:32 +0530

    Add files via upload

--- home.java
+++ home.java
@@ -4,6 +4,10 @@ import java.awt.*;
 import java.sql.*;
 import java.awt.event.*;
 
+//classpath
+//CLASSPATH=$CLASSPATH:/usr/share/java/mysql.jar
+//export CLASSPATH
+
 class Frame extends JFrame{
 
   private JLabel label1,label2,label3;
@@ -64,7 +68,7 @@ class itemPage extends JPanel{
         System.out.println(e);
       }
       String[] cols = {"Name","Quantity","Price"};
-      Object[][] data = new Object[3][l];
+      Object[][] data = new Object[l][3];
       try{
         Class.forName("com.mysql.jdbc.Driver");
         Connection con2 = DriverManager.getConnection("jdbc:mysql://localhost/store","root","toor");
@@ -86,25 +90,27 @@ class itemPage extends JPanel{
       JTable table = new JTable(data,cols);
       JScrollPane jsp = new JScrollPane(table);
       add(jsp,c1);
-      //code ends here
+
     }
 
-    // public void view_item_page(){
-    //   try{
-    //     Class.forName("com.mysql.jdbc.Driver");
-    //     Connection con2 = DriverManager.getConnection("jdbc:mysql://localhost/store","root","toor");
-    //     Statement query2 = con2.createStatement();
-    //     ResultSet result2 = query2.executeQuery("SELECT * FROM products");
-    //     while(result2.next()){
-    //       String name = result2.getString("name");
-    //       int quantity = result2.getInt("quantity");
-    //       int price = result2.getInt("mrp");
-    //       System.out.println(name+" - "+quantity+" - "+price);
-    //     }
-    //   }catch(Exception e){
-    //     System.out.println(e);
-    //   }
-    // }
+
+
+    public void view_item_page(){
+      try{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con2 = DriverManager.getConnection("jdbc:mysql://localhost/store","root","toor");
+        Statement query2 = con2.createStatement();
+        ResultSet result2 = query2.executeQuery("SELECT * FROM products");
+        while(result2.next()){
+          String name = result2.getString("name");
+          int quantity = result2.getInt("quantity");
+          int price = result2.getInt("mrp");
+          System.out.println(name+" - "+quantity+" - "+price);
+        }
+      }catch(Exception e){
+        System.out.println(e);
+      }
+    }
 
 }
 
@@ -149,7 +155,7 @@ class addItem extends JPanel{
           String field1_value = field1.getText();
           String field2_value = field2.getText();
           String field3_value = field3.getText();
-          JOptionPane.showMessageDialog(null, field1_value+" - "+field2_value+" - "+field3_value);
+          JOptionPane.showMessageDialog(null, field3_value+" "+field1_value+" worth Rs. "+field2_value+" have been added");
           add_item_sql(field1_value,field2_value,field3_value);
         }
       });
@@ -197,7 +203,40 @@ class transaction extends JPanel{
     add(field3,c1);
     c1.gridy++;
     add(b1,c1);
+    b1.addActionListener(new ActionListener(){
+      public void actionPerformed(ActionEvent ae){
+        String first = field1.getText();
+        String second = field3.getText();
+        JOptionPane.showMessageDialog(null,second+" "+first+" have been sold");
+        sold_items(first,second);
+
+      }
+    });
   }
+
+  public void sold_items(String name2, String qty){
+    int qty_new = Integer.parseInt(qty);
+    int previous_Stock = 0;
+    try{
+      Class.forName("com.mysql.jdbc.Driver");
+      Connection con = DriverManager.getConnection("jdbc:mysql://localhost/store","root","toor");
+      Statement query1 = con.createStatement();
+      Statement query0 = con.createStatement();
+      ResultSet rs = query0.executeQuery("SELECT quantity FROM products WHERE name = '"+name2+"'");
+      while(rs.next()){
+           previous_Stock = rs.getInt("quantity");
+      }
+      int new_stock = previous_Stock - qty_new ;
+      query1.executeUpdate("UPDATE products SET quantity = '"+new_stock+"' WHERE name = '"+name2+"'");
+      }catch(SQLException se){
+        se.printStackTrace();
+      }
+      catch(Exception e){
+        System.out.println(e);
+      }
+    }
+
+
 }
 
 
